91944fb977c7496a4c125a615b74687efd04859e
Martin Morys <Martin.Morys@gmail.com>
2019-08-08 07:35:09 +0200

    new structure -> seperation into three repos

--- events.go
+++ events.go
@@ -18,21 +18,32 @@ const (
 )
 
 var eventText = map[ProxyEventType]string{
-	EventCreatingConnection:		"Creating connection",
-	EventProcessingRequest:			"Processing request",
-	EventNtlmAuthRequestDetected: 	"NTLM authentication request detected",
-	EventRecievedAuthToken:			"Recieved authentication token",
-	EventSendingAuthToken: 			"Sending authentication token",
-	EventSendingRequest: 			"Sending request to proxy server",
-	EventPeekedResponse: 			"Peeking response from proxy server",
-	EventRecievedResponse: 			"Recieved response from proxy server",
-	EventConnectionEstablished:		"Connection with to proxy server ist established",
-	EventConnectionClosed:			"Connection ist closed",
+	EventCreatingConnection:      "Creating connection",
+	EventProcessingRequest:       "Processing request",
+	EventNtlmAuthRequestDetected: "NTLM authentication request detected",
+	EventRecievedAuthToken:       "Recieved authentication token",
+	EventSendingAuthToken:        "Sending authentication token",
+	EventSendingRequest:          "Sending request to proxy server",
+	EventPeekedResponse:          "Peeking response from proxy server",
+	EventRecievedResponse:        "Recieved response from proxy server",
+	EventConnectionEstablished:   "Connection with to proxy server ist established",
+	EventConnectionClosed:        "Connection ist closed",
 }
 
 //A ProxyEventListener will be notified in the case of an event
 type ProxyEventListener interface {
 	OnProxyEvent(t ProxyEventType, pc *ProxyCommunication)
+}
+
+//EventData contains corresponding informations for events
+type EventData struct {
+	EventType        ProxyEventType
+	
+	ClientHost       string
+	ProxyHost        string
+	CurrentAuthToken string
+	RequestedURI     string
+	Communication    ProxyCommunication
 }
 
 //EventText returns text for event type
--- go.mod
+++ go.mod
@@ -1,17 +1,5 @@
 module github.com/Neothorn23/proxyproxy
 
-require (
-	github.com/alexbrainman/sspi v0.0.0-20180613141037-e580b900e9f5
-	github.com/apex/log v1.1.0
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fatih/color v1.7.0 // indirect
-	github.com/lxn/walk v0.0.0-20190619151032-86d8802c197a // indirect
-	github.com/lxn/win v0.0.0-20190716185335-d1d36f0e4f48 // indirect
-	github.com/mattn/go-colorable v0.1.0 // indirect
-	github.com/mattn/go-isatty v0.0.4 // indirect
-	github.com/pkg/errors v0.8.1
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.2.2 // indirect
-	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a // indirect
-	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
-)
+go 1.12
+
+require github.com/apex/log v1.1.1 // indirect
